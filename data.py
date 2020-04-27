6679cd8667e24da94bdde2509ee98d0828cf6555
tpham393 <thuyp@bu.edu>
2020-04-27 12:49:23 -0400

    test shape

--- data.py
+++ data.py
@@ -30,6 +30,8 @@ def split_B(B_tuple_img_tensor):
 def uncompress(embedding):
     embedding = keras.layers.Dense(tf.keras.backend.prod(embedding.shape))(embedding)
     embedding = tf.reshape(embedding, (1, 32, 32, 3)) # unflatten
+    print(embedding.shape)
+    
     return embedding
 
 def make_dataset(img_paths, batch_size, load_size, crop_size, training, drop_remainder=True, shuffle=True, repeat=1):
